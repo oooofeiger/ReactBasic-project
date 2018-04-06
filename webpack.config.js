798ca42ae3e1de5a710c18d5e73ace6a09e830cf
oooofeiger <1014577169@qq.com>
2018-04-06 23:35:58 +0800

    splite code

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 var webpack = require('webpack');
 var path = require('path');
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+var ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+// var cssExtract = new ExtractTextWebpackPlugin('css.css');
 
 module.exports = {
 	entry:'./src/js/root.js',
@@ -28,20 +31,39 @@ module.exports = {
 			},
 			{
 				test:/\.css$/,
-				use:[
-					{loader:'style-loader'},
-					{
-						loader:'css-loader',
-						// options:{
-						// 	modules:true,
-						// 	localIdentName:'[path][name]_[local]--[hash:base64:5]',
-						// 	importLoaders:1
-						// }
-					}
-				]
+				use:ExtractTextWebpackPlugin.extract({
+					use:'css-loader',
+					fallback:'style-loader'
+				})
 			}
 		]
 	},
+	optimization: {
+	     splitChunks: {
+	     chunks: "all", // 必须三选一： "initial" | "all"(默认就是all) | "async" 
+	     minSize: 0, // 最小尺寸，默认0
+	     minChunks: 1, // 最小 chunk ，默认1
+	     maxAsyncRequests: 1, // 最大异步请求数， 默认1
+	     maxInitialRequests : 1, // 最大初始化请求书，默认1
+	     cacheGroups:{ // 这里开始设置缓存的 chunks
+	         priority: '0', // 缓存组优先级
+	         vendor: { // key 为entry中定义的 入口名称
+	             chunks: "initial", // 必须三选一： "initial" | "all" | "async"(默认就是异步) 
+	             test: /react|lodash/, // 正则规则验证，如果符合就提取 chunk
+	             name: "vendor", // 要缓存的 分隔出来的 chunk 名称 
+	             minSize: 0,
+	             minChunks: 1,
+	             enforce: true,
+	             maxAsyncRequests: 1, // 最大异步请求数， 默认1
+	             maxInitialRequests : 1, // 最大初始化请求书，默认1
+	             reuseExistingChunk: true // 可设置是否重用该chunk（查看源码没有发现默认值）
+	        	 }
+	     	}
+ 	 	}
+ 	},
+ 	plugins: [
+ 		new ExtractTextWebpackPlugin('css/style.css')
+	],
 
 	output:{
 		path:__dirname+'/src/',
