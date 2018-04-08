8ceecb7ae494721755b492cbcb637dc533a8e9d6
oooofeiger <1014577169@qq.com>
2018-04-08 19:23:36 +0800

    modified uri

--- app.js
+++ app.js
@@ -3,12 +3,6 @@ var path = require('path');
 var app = new express();
 
 app.use(express.static(__dirname))
-app.all('*', function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "X-Requested-With");
-	res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-	res.header("X-Powered-By",' 3.2.1');
-	res.header("Content-Type", "application/json;charset=utf-8");
-});
+
 app.listen(8080);
 console.log('app is running at port 8080')
