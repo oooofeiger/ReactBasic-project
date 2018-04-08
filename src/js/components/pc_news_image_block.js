26dc433e6d6af92149a39c224b38f1d2865307d8
oooofeiger <1014577169@qq.com>
2018-04-08 12:42:18 +0800

    modified url

--- src/js/components/pc_news_image_block.js
+++ src/js/components/pc_news_image_block.js
@@ -16,7 +16,7 @@ export default class PCImageBlock extends React.Component {
       method: 'GET',
       mode: 'cors'
     };
-    fetch('http://localhost:8080/juhe/toutiao/index?type='+this.props.type+'&key=ef4a86a03b270aa4be489573bf3f31dd')
+    fetch('http://v.juhe.cn/toutiao/index?type='+this.props.type+'&key=ef4a86a03b270aa4be489573bf3f31dd')
     .then(response => response.json())
     .then(json => {
       if(this._isMounted){
