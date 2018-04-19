5046239f0c729f02ad4ad5a2f92dd75080b90a33
oooofeiger <101457769@qq.com>
2018-04-19 17:17:25 +0800

    update details

--- src/js/components/mobile_news_details.js
+++ src/js/components/mobile_news_details.js
@@ -16,7 +16,9 @@ export default class PCNewsDetails extends React.Component{
     fetch('http://www.feiger.com.cn/Handler.ashx?action=getnewsitem&uniquekey=' + this.props.match.params.uniquekey)
     .then(res=>res.json())
     .then(json => {
-      this.setState({newsItem: json});
+      var div = document.createElement('div');
+      div.innerHTML = json.pagecontent;
+      this.setState({newsItem: div.childNodes[0]});
       document.title = this.state.newsItem.title + ' - React News | React 驱动的新闻平台';
     })
   }
@@ -32,7 +34,7 @@ export default class PCNewsDetails extends React.Component{
         <Row>
           <Col span={2}></Col>
           <Col span={14} class='container'>
-            <div class='articleContainer' dangerousSetInnerHTML={this.createMarkup()}></div>
+            <div>{this.state.newsItem}</div>
           </Col>
           <Col span={6}></Col>
           <Col span={2}></Col>
