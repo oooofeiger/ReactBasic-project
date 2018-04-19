5a8989b9e9d9664e1d6ca9e9c947a81bbc8acafe
oooofeiger <101457769@qq.com>
2018-04-19 15:36:26 +0800

    dev

--- src/js/root.js
+++ src/js/root.js
@@ -24,7 +24,7 @@ class Root extends React.Component{
           </MediaQuery>
           <MediaQuery query="(max-device-width:1224px)">
             <Route exact path='/' component={MobileIndex}></Route>
-            <Route path="/details/:uniquekey" component={PCNewsDetails}></Route>
+            <Route path="/details/:uniquekey" component={MobileNewsDetails}></Route>
             <Route path='/usercenter' component={MobileUserCenter}></Route>
           </MediaQuery>
         </div>
