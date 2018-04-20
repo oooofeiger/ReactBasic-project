5e00dbb1dec9f318038fb9d805a58422e958ef4f
oooofeiger <101457769@qq.com>
2018-04-20 11:24:11 +0800

    replace BrowserRouter to HashRouter

--- webpack.config.js
+++ webpack.config.js
@@ -53,9 +53,9 @@ module.exports = {
 	     maxInitialRequests : 1, // 最大初始化请求书，默认1
 	     cacheGroups:{ // 这里开始设置缓存的 chunks
 	         priority: '0', // 缓存组优先级
-	         vendor: { // key 为entry中定义的 入口名称
-	             chunks: "initial", // 必须三选一： "initial" | "all" | "async"(默认就是异步)
-	             test: /react|lodash/, // 正则规则验证，如果符合就提取 chunk
+	         app: { // key 为entry中定义的 入口名称
+	             chunks: "all", // 必须三选一： "initial" | "all" | "async"(默认就是异步)
+	             test: /(react|lodash)/, // 正则规则验证，如果符合就提取 chunk
 	             name: "vendor", // 要缓存的 分隔出来的 chunk 名称
 	             minSize: 0,
 	             minChunks: 1,
